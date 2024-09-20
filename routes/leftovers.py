bfe6bbc7d626c0514ebfa15a0912048b888968c8
Toru4ka <ilya.myhin.2001@gmail.com>
2024-09-20 23:24:00 +0300

    feat: add new modal window for leftovers were inside 3 buttons for 2 warehouses Мытищи and Вёшки and third button for unloading leftovers from all warehouses

--- routes/leftovers.py
+++ routes/leftovers.py
@@ -12,10 +12,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 OUTPUT_FILES_DIR_LEFTOVERS = BASE_DIR / 'leftovers' / 'output_files'
 
 
-@router.get('/venera-carpet-leftovers')
+@router.get('/venera-carpet-leftovers/mytishchi')
 async def generate(background_tasks: BackgroundTasks):
+    warehouse = "?warehouses%5B%5D=4"
     try:
-        await asyncio.to_thread(run_generation)
+        await asyncio.to_thread(run_generation, warehouse)
         output_file = OUTPUT_FILES_DIR_LEFTOVERS / 'leftovers.csv'
         if output_file.exists():
             background_tasks.add_task(clear_folder, OUTPUT_FILES_DIR_LEFTOVERS)
@@ -26,3 +27,38 @@ async def generate(background_tasks: BackgroundTasks):
             raise HTTPException(status_code=500, detail="File not found after processing")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.get('/venera-carpet-leftovers/vyoshki')
+async def generate(background_tasks: BackgroundTasks):
+    warehouse = "?warehouses%5B%5D=80"
+    try:
+        await asyncio.to_thread(run_generation, warehouse)
+        output_file = OUTPUT_FILES_DIR_LEFTOVERS / 'leftovers.csv'
+        if output_file.exists():
+            background_tasks.add_task(clear_folder, OUTPUT_FILES_DIR_LEFTOVERS)
+            return FileResponse(path=output_file,
+                                filename='leftovers.csv',
+                                media_type='text/csv')
+        else:
+            raise HTTPException(status_code=500, detail="File not found after processing")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.get('/venera-carpet-leftovers/all_warehouses')
+async def generate(background_tasks: BackgroundTasks):
+    warehouse = "?warehouses%5B%5D=4&warehouses%5B%5D=80&warehouseType=many"
+    try:
+        await asyncio.to_thread(run_generation, warehouse)
+        output_file = OUTPUT_FILES_DIR_LEFTOVERS / 'leftovers.csv'
+        if output_file.exists():
+            background_tasks.add_task(clear_folder, OUTPUT_FILES_DIR_LEFTOVERS)
+            return FileResponse(path=output_file,
+                                filename='leftovers.csv',
+                                media_type='text/csv')
+        else:
+            raise HTTPException(status_code=500, detail="File not found after processing")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
